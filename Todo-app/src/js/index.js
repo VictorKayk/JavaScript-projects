b8c6de639e7a883598a15e81912b0d632e98dc81
Victor Kayk Da Silva <victorkayk77@outlook.com>
2021-08-16 22:07:32 -0300

    feat(temporarily gloal): finalizing the theme changer
    n

--- Todo-app/src/js/index.js
+++ Todo-app/src/js/index.js
@@ -1,28 +1,42 @@
 // import toggleTheme from './modules/toggle-theme-color.js';
 
+const body = document.querySelector('body');
 const buttonThemeChanger = document.body.querySelector('#theme-changer');
-const imgThemeChanger = document.body.querySelector('#theme-changer img');
+const iconThemeChanger = document.body.querySelector('#theme-changer img');
 
-function initialTheme() {
-  localStorage.theme = localStorage.theme || 'light';
+function toggleIconTheme(iconThemeTag) {
+  const imgButton = iconThemeTag.getAttribute('src');
+  if (imgButton.includes('dark')) iconThemeTag.setAttribute('src', imgButton.replace('dark', 'light'));
+  else if (imgButton.includes('light')) iconThemeTag.setAttribute('src', imgButton.replace('light', 'dark'));
 }
 
-initialTheme();
+function removeTheThemeClassOfBody() {
+  body.classList.remove('light');
+  body.classList.remove('dark');
+}
 
-function isTheCorrectTheme(imgTagTheme) {
-  const imgButton = imgTagTheme.getAttribute('src');
+function putTheThemeClassOnBody() {
+  body.classList.add(localStorage.theme);
+}
+
+function toggleThemeInBody() {
+  removeTheThemeClassOfBody();
+  putTheThemeClassOnBody();
+}
+
+function initialTheme(iconThemeTag) {
+  localStorage.theme = localStorage.theme || 'light';
+
+  toggleIconTheme(iconThemeTag);
+  toggleThemeInBody();
+}
+
+initialTheme(iconThemeChanger);
+
+function isTheCorrectTheme(iconThemeTag) {
+  const imgButton = iconThemeTag.getAttribute('src');
   if (imgButton.includes('light')) return localStorage.theme === 'light';
   return localStorage.theme === 'dark';
-}
-
-function toggleImgTheme(imgTagTheme) {
-  const imgButton = imgTagTheme.getAttribute('src');
-  if (imgButton.includes('dark')) imgTagTheme.setAttribute('src', imgButton.replace('dark', 'light'));
-  else if (imgButton.includes('light')) imgTagTheme.setAttribute('src', imgButton.replace('light', 'dark'));
-}
-
-function toggleTheme(imgTagTheme) {
-  toggleImgTheme(imgTagTheme);
 }
 
 function toggleLocalStorageTheme() {
@@ -33,12 +47,13 @@ function toggleLocalStorageTheme() {
   localStorage.theme = possibleChoices[localStorage.theme];
 }
 
-if (!isTheCorrectTheme(imgThemeChanger)) toggleImgTheme(imgThemeChanger);
+if (!isTheCorrectTheme(iconThemeChanger)) toggleIconTheme(iconThemeChanger);
 
 window.addEventListener('click', (e) => {
   const { target } = e;
   if (target.getAttribute('data-js')) {
     toggleLocalStorageTheme();
-    toggleTheme(imgThemeChanger);
+    toggleIconTheme(iconThemeChanger);
+    toggleThemeInBody();
   }
 });
