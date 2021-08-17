6b9aa8978d4cb6925214da862ded47b340502443
Victor Kayk Da Silva <victorkayk77@outlook.com>
2021-08-17 02:31:31 -0300

    refactor(index and style): improving the structure of the code

--- Todo-app/src/js/index.js
+++ Todo-app/src/js/index.js
@@ -3,21 +3,3 @@ import themeChanger from './modules/toogle-theme-color.js';
 const iconThemeChanger = document.body.querySelector('#theme-changer img');
 
 themeChanger(iconThemeChanger);
-
-window.addEventListener('mouseover', (e) => {
-  const { target } = e;
-  if (target.classList.contains('task')) {
-    const { children } = target;
-    const btn = children[1];
-    btn.classList.add('hover');
-  }
-});
-
-window.addEventListener('mouseout', (e) => {
-  const { target } = e;
-  if (target.classList.contains('task')) {
-    // console.log(target.children);
-    const btn = target.querySelector('button.btn');
-    btn.classList.remove('hover');
-  }
-});
